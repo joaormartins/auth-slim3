816862e3496251e73eded1c0e8d559872e50bb27
Joao Martins Junior <joaodrmjr@gmail.com>
2020-07-07 14:38:10 -0300

    Old Input data middleware

---
+++ app/Middleware/OldInputMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Middleware;
+
+class OldInputMiddleware extends Middleware {
+
+	public function __invoke($request, $response, $next)
+	{
+		if (isset($_SESSION["old"])) {
+			$this->container->view->getEnvironment()->addGlobal("old", $_SESSION["old"]);
+		}
+		$_SESSION["old"] = $request->getParams();
+
+		return $next($request, $response);
+
+	}
+}
--- app/middleware.php
+++ app/middleware.php
@@ -2,3 +2,5 @@
 // Application middleware
 
 // $app->add(new \Slim\Csrf\Guard());
+
+$app->add(new App\Middleware\OldInputMiddleware($container));
