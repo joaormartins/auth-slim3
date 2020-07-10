b72d439bcba2dfc9ebffb9a15231a3af7036063b
Joao Martins Junior <joaodrmjr@gmail.com>
2020-07-10 08:09:54 -0300

    Modification in Remember middleware

--- app/Middleware/RememberMiddleware.php
+++ app/Middleware/RememberMiddleware.php
@@ -8,27 +8,24 @@ class RememberMiddleware extends Middleware {
 
 	public function __invoke($request, $response, $next)
 	{
-		$this->check();
-
-		return $next($request, $response);
-
-	}
-
-	protected function check()
-	{
 		$rememberCookie = $this->container->settings["auth"]["remember"];
 		if (!$this->container->auth->loginState() && isset($_COOKIE[$rememberCookie])) {
 			$hash = $_COOKIE[$rememberCookie];
 			$uagent = userAgentNoVersion();
 			$uSession = UserSession::where("session", $hash)->where("user_agent", $uagent)->first();
 			if ($uSession && $user = User::find($uSession->user_id)) {
-				if ($uSession->expiry < time()) {
-					$uSession->delete();
-					setcookie($rememberCookie, null, time()-1, '/');
-					return;
+				if ($uSession->expiry > time()) {
+					$this->container->auth->login($user);
+					$this->container->flash->addMessage("info", "Seja bem vindo {$user->username}");
+					return $response->withRedirect($this->container->router->pathFor("home"));
 				}
-				$this->container->auth->login($user);
+
+				$uSession->delete();
+				setcookie($rememberCookie, null, time()-1, '/');
 			}
 		}
+
+		return $next($request, $response);
+
 	}
 }
